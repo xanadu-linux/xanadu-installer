41225a08428899b9367cb0a94d5e9a23feb350a5
jose perez <sinfallas@gmail.com>
2015-12-04 23:29:22 -0430

    * se elimina la dependencia del archivo variables

--- refractainstaller-wrapper.sh
+++ refractainstaller-wrapper.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 LC_ALL=C
-source /usr/bin/variables
+advertencia='yad --skip-taskbar --button=ACEPTAR:1 --fixed --center --text-align=fill --borders=6 --title="ADVERTENCIA"'
+memoria=$(grep "MemTotal" /proc/meminfo | awk '{print $2}')
+cpu=$(lscpu | grep "MHz" | awk '{print $3}')
 if ! [[ -d /lib/live/mount/rootfs ]]; then
 	exit 1
 fi
@@ -10,7 +12,6 @@ fi
 if (( $cpu < 1000.000 )); then
 	"$advertencia" --timeout=10 --text="Su CPU no es capaz de brindar una experiencia optima para ejecutar Xanadu GNU/Linux."
 fi
-
 wget -q -T 2 -O - https://raw.githubusercontent.com/sinfallas/xanadu-installer/master/refractainstaller-yad | bash /dev/stdin
 if [[ $? != 0 ]]; then
 	gksu 'x-terminal-emulator -e /usr/bin/refractainstaller-yad' &
