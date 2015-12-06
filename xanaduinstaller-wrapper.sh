5368b157fe2cb1a4371e6005cfc7ecbb06e8239e
jose perez <sinfallas@gmail.com>
2015-12-05 19:54:29 -0430

    * se corrige un error en la comprobacion del cpu * se traslada la descarga del script al instalador

--- xanaduinstaller-wrapper.sh
+++ xanaduinstaller-wrapper.sh
@@ -5,17 +5,15 @@ LC_ALL=C
 advertencia='yad --skip-taskbar --button="ACEPTAR":1 --fixed --center --text-align=fill --borders=6 --title="ADVERTENCIA"'
 memoria=$(grep "MemTotal" /proc/meminfo | awk '{print $2}')
 cpu=$(lscpu | grep "MHz" | awk '{print $3}')
+cpu2=${cpu%.*}
 if ! [[ -d /lib/live/mount/rootfs ]]; then
 	exit 1
 fi
 if (( $memoria < 512000 )); then
 	$advertencia --timeout=10 --text="Necesita al menos 512 MB de RAM para disfrutar de una experiencia optima en Xanadu GNU/Linux."
 fi
-if (( $cpu < 1000.000 )); then
+if (( $cpu2 < 1000 )); then
 	$advertencia --timeout=10 --text="Su CPU no es capaz de brindar una experiencia optima para ejecutar Xanadu GNU/Linux."
 fi
-gksu 'x-terminal-emulator -e wget -q -T 2 -O - https://raw.githubusercontent.com/sinfallas/xanadu-installer/master/xanaduinstaller-yad | bash /dev/stdin' &
-if [[ $? != 0 ]]; then
-	gksu 'x-terminal-emulator -e /usr/bin/xanaduinstaller-yad' &
-fi
+gksu 'x-terminal-emulator -e /usr/bin/xanaduinstaller-yad' &
 exit 0
